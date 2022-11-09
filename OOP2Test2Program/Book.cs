6ae29f7ad9eb7d3d9f402c9380beaed32dfee740
Arun <arunkarki252@gmail.com>
2022-11-09 09:35:31 -0500

    Task 4

---
+++ OOP2Test2Program/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2Test2Program
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+
+        public string Burn()
+        {
+            return Title;
+        }
+    }
+}
---
+++ OOP2Test2Program/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2Test2Program
+{
+    internal class Text: Book
+    {
+        public int Length { get; }
+        public string Content;
+    }
+}
